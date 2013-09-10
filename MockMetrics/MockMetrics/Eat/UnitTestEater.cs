6f9b2a9f266ee92bde3853599e800273a2a1e319
KozhevnikovDmitry <d.o.kozhevnikov@gmail.com>
2013-09-10 17:05:53 +0800

    [+] add some more empty concept classes

---
+++ MockMetrics/MockMetrics/Eat/UnitTestEater.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace MockMetrics.Eat
+{
+    public interface IEater<T> where T : ICSharpTreeNode
+    {
+        Snapshot Eat(Snapshot snapshot, IMethodDeclaration unitTest, T parameterDeclaration);
+    }
+
+    public class UnitTestEater 
+    {
+        public Snapshot EatUnitTest(IMethodDeclaration unitTest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RegularParameterEater : IEater<IRegularParameterDeclaration>
+    {
+        public Snapshot Eat(Snapshot snapshot, IMethodDeclaration unitTest, IRegularParameterDeclaration parameterDeclaration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BlockEater : IEater<IRegularParameterDeclaration>
+    {
+        public Snapshot Eat(Snapshot snapshot, IMethodDeclaration unitTest, IRegularParameterDeclaration parameterDeclaration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PostEater
+    {
+        public void PostEat(Snapshot snapshot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
