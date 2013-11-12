1d5021b802f2f3754bca2fa8517277f22b557528
KozhevnikovDmitry <d.o.kozhevnikov@gmail.com>
2013-11-12 11:25:28 +0800

    [+] EmptyStatementEater is implemented

---
+++ MockMetrics/MockMetrics.Eating/Statement/EmptyStatementEater.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace MockMetrics.Eating.Statement
+{
+    public class EmptyStatementEater : StatementEater<IEmptyStatement>
+    {
+        public EmptyStatementEater(IEater eater) : base(eater)
+        {
+        }
+
+        public override void Eat(ISnapshot snapshot, IEmptyStatement statement)
+        {
+            
+        }
+    }
+}
