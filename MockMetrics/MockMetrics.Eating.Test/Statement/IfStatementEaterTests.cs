857ed65ab4528ca41dc71e07b9a16de358d93e3c
darkbasil <basil_triste@mail.ru>
2013-11-12 16:37:20 +0800

    [+] IfStatementEater is implemented

---
+++ MockMetrics/MockMetrics.Eating.Test/Statement/IfStatementEaterTests.cs
@@ -0,0 +1,80 @@
+﻿using JetBrains.ReSharper.Psi.CSharp.Tree;
+using MockMetrics.Eating.Statement;
+using NUnit.Framework;
+using Moq;
+
+namespace MockMetrics.Eating.Test.Statement
+{
+	[TestFixture]
+	public class IfStatementEaterTests
+	{
+		[Test]
+		public void EatThenTest()
+		{
+			// Arrange
+			var snapshot = Mock.Of<ISnapshot>();
+			var body = Mock.Of<ICSharpStatement>();
+			var ifStatement = Mock.Of<IIfStatement>(t => t.Then == body);
+			var eater = new Mock<IEater>();
+			var ifStatementEater = new IfStatementEater(eater.Object);
+
+			// Act
+			ifStatementEater.Eat(snapshot, ifStatement);
+
+			// Assert
+			eater.Verify(t => t.Eat(snapshot, body), Times.Once);
+		}
+
+		[Test]
+		public void EatElseTest()
+		{
+			// Arrange
+			var snapshot = Mock.Of<ISnapshot>();
+			var body = Mock.Of<ICSharpStatement>();
+			var ifStatement = Mock.Of<IIfStatement>(t => t.Else == body);
+			var eater = new Mock<IEater>();
+			var ifStatementEater = new IfStatementEater(eater.Object);
+
+			// Act
+			ifStatementEater.Eat(snapshot, ifStatement);
+
+			// Assert
+			eater.Verify(t => t.Eat(snapshot, body), Times.Once);
+		}
+
+		[Test]
+		public void EatConditionTest()
+		{
+			// Arrange
+			var snapshot = Mock.Of<ISnapshot>();
+			var condition = Mock.Of<ICSharpExpression>();
+			var ifStatement = Mock.Of<IIfStatement>(t => t.Condition == condition);
+			var eater = new Mock<IEater>();
+			var ifStatementEater = new IfStatementEater(eater.Object);
+
+			// Act
+			ifStatementEater.Eat(snapshot, ifStatement);
+
+			// Assert
+			eater.Verify(t => t.Eat(snapshot, condition), Times.Once);
+		}
+
+		[Test]
+		public void AddConditionToSnapshotTest()
+		{
+			// Arrange
+			var snapshot = new Mock<ISnapshot>();
+			var condition = Mock.Of<ICSharpExpression>();
+			var ifStatement = Mock.Of<IIfStatement>(t => t.Condition == condition);
+
+			var eater = Mock.Of<IEater>(t => t.Eat(snapshot.Object, condition) == ExpressionKind.None);
+			var ifStatementEater = new IfStatementEater(eater);
+
+			// Act
+			ifStatementEater.Eat(snapshot.Object, ifStatement);
+
+			// Assert
+			snapshot.Verify(t => t.AddTreeNode(ExpressionKind.None, condition), Times.Once);
+		} 
+	}
+}
---
+++ MockMetrics/MockMetrics.Eating/Statement/IfStatementEater.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace MockMetrics.Eating.Statement
+{
+	public class IfStatementEater : StatementEater<IIfStatement>
+	{
+		public IfStatementEater(IEater eater)
+			: base(eater)
+		{			
+		}
+
+		public override void Eat(ISnapshot snapshot, IIfStatement statement)
+		{
+			Eater.Eat(snapshot, statement.Then);
+			if (statement.Else != null)
+			{
+				Eater.Eat(snapshot, statement.Else);
+			}			
+
+			var condKind = Eater.Eat(snapshot, statement.Condition);
+			snapshot.AddTreeNode(condKind, statement.Condition);		
+		}
+	}
+}
