7c69bec18332d6d34cb8a4c4f8957935a3ff7767
KozhevnikovDmitry <d.o.kozhevnikov@gmail.com>
2013-11-12 10:12:53 +0800

    [+] ForStatementEater implemented

---
+++ MockMetrics/MockMetrics.Eating.Test/Statement/ForStatementEaterTests.cs
@@ -0,0 +1,173 @@
+﻿using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Tree;
+using MockMetrics.Eating.Statement;
+using Moq;
+using NUnit.Framework;
+
+namespace MockMetrics.Eating.Test.Statement
+{
+    [TestFixture]
+    public class ForStatementEaterTests
+    {
+        [Test]
+        public void EatBodyTest()
+        {
+            // Arrange
+            var snapshot = Mock.Of<ISnapshot>();
+            var body = Mock.Of<ICSharpStatement>();
+            var forStatement = Mock.Of<IForStatement>(t => t.Body == body);
+            Mock.Get(forStatement)
+                .Setup(t => t.Initializer.Expressions)
+                .Returns(new TreeNodeCollection<ICSharpExpression>(new ICSharpExpression[0]));
+            Mock.Get(forStatement)
+                .Setup(t => t.Iterators.Expressions)
+                .Returns(new TreeNodeCollection<ICSharpExpression>(new ICSharpExpression[0]));
+            var eater = new Mock<IEater>();
+            var forEater = new ForStatementEater(eater.Object);
+
+            // Act
+            forEater.Eat(snapshot, forStatement);
+
+            // Assert
+            eater.Verify(t => t.Eat(snapshot, body), Times.Once);
+        }
+
+        [Test]
+        public void EatConditionTest()
+        {
+            // Arrange
+            var snapshot = Mock.Of<ISnapshot>();
+            var condition = Mock.Of<ICSharpExpression>();
+            var forStatement = Mock.Of<IForStatement>(t => t.Condition == condition);
+            Mock.Get(forStatement)
+                .Setup(t => t.Initializer.Expressions)
+                .Returns(new TreeNodeCollection<ICSharpExpression>(new ICSharpExpression[0]));
+            Mock.Get(forStatement)
+                .Setup(t => t.Iterators.Expressions)
+                .Returns(new TreeNodeCollection<ICSharpExpression>(new ICSharpExpression[0]));
+            var eater = new Mock<IEater>();
+            var forEater = new ForStatementEater(eater.Object);
+
+            // Act
+            forEater.Eat(snapshot, forStatement);
+
+            // Assert
+            eater.Verify(t => t.Eat(snapshot, condition), Times.Once);
+        }
+
+        [Test]
+        public void AddConditionToSnapshotTest()
+        {
+            // Arrange
+            var snapshot = new Mock<ISnapshot>();
+            var condition = Mock.Of<ICSharpExpression>();
+            var forStatement = Mock.Of<IForStatement>(t => t.Condition == condition && t.Initializer == Mock.Of<IForInitializer>());
+            Mock.Get(forStatement)
+                .Setup(t => t.Initializer.Expressions)
+                .Returns(new TreeNodeCollection<ICSharpExpression>(new ICSharpExpression[0]));
+            Mock.Get(forStatement)
+                .Setup(t => t.Iterators.Expressions)
+                .Returns(new TreeNodeCollection<ICSharpExpression>(new ICSharpExpression[0]));
+            var eater = Mock.Of<IEater>(t => t.Eat(snapshot.Object, condition) == ExpressionKind.None);
+            var forEater = new ForStatementEater(eater);
+
+            // Act
+            forEater.Eat(snapshot.Object, forStatement);
+
+            // Assert
+            snapshot.Verify(t => t.AddTreeNode(ExpressionKind.None, condition), Times.Once);
+        }
+
+        [Test]
+        public void EatInitializerTest()
+        {
+            // Arrange
+            var snapshot = Mock.Of<ISnapshot>();
+            var initilizer = Mock.Of<ICSharpExpression>();
+            var forStatement = Mock.Of<IForStatement>();
+            Mock.Get(forStatement)
+               .Setup(t => t.Initializer.Expressions)
+               .Returns(new TreeNodeCollection<ICSharpExpression>(new[] { initilizer }));
+            Mock.Get(forStatement)
+                .Setup(t => t.Iterators.Expressions)
+                .Returns(new TreeNodeCollection<ICSharpExpression>(new ICSharpExpression[0]));
+            var eater = new Mock<IEater>();
+            var forEater = new ForStatementEater(eater.Object);
+
+            // Act
+            forEater.Eat(snapshot, forStatement);
+
+            // Assert
+            eater.Verify(t => t.Eat(snapshot, initilizer), Times.Once);
+        }
+
+        [Test]
+        public void AddInitializerToSnapshotTest()
+        {
+            // Arrange
+            var snapshot = new Mock<ISnapshot>();
+            var initilizer = Mock.Of<ICSharpExpression>();
+            var forStatement = Mock.Of<IForStatement>();
+            Mock.Get(forStatement)
+               .Setup(t => t.Initializer.Expressions)
+               .Returns(new TreeNodeCollection<ICSharpExpression>(new[] { initilizer }));
+            Mock.Get(forStatement)
+                .Setup(t => t.Iterators.Expressions)
+                .Returns(new TreeNodeCollection<ICSharpExpression>(new ICSharpExpression[0]));
+            var eater = Mock.Of<IEater>(t => t.Eat(snapshot.Object, initilizer) == ExpressionKind.None);
+            var forEater = new ForStatementEater(eater);
+
+            // Act
+            forEater.Eat(snapshot.Object, forStatement);
+
+            // Assert
+            snapshot.Verify(t => t.AddTreeNode(ExpressionKind.None, initilizer), Times.Once);
+        }
+
+        [Test]
+        public void EatIteratorsTest()
+        {
+            // Arrange
+            var snapshot = Mock.Of<ISnapshot>();
+            var iterator = Mock.Of<ICSharpExpression>();
+            var forStatement = Mock.Of<IForStatement>();
+            Mock.Get(forStatement)
+               .Setup(t => t.Initializer.Expressions)
+               .Returns(new TreeNodeCollection<ICSharpExpression>(new ICSharpExpression[0]));
+            Mock.Get(forStatement)
+                .Setup(t => t.Iterators.Expressions)
+                .Returns(new TreeNodeCollection<ICSharpExpression>(new[] { iterator }));
+            var eater = new Mock<IEater>();
+            var forEater = new ForStatementEater(eater.Object);
+
+            // Act
+            forEater.Eat(snapshot, forStatement);
+
+            // Assert
+            eater.Verify(t => t.Eat(snapshot, iterator), Times.Once);
+        }
+
+        [Test]
+        public void AdditeratorsToSnapshotTest()
+        {
+            // Arrange
+            var snapshot = new Mock<ISnapshot>();
+            var iterator = Mock.Of<ICSharpExpression>();
+            var forStatement = Mock.Of<IForStatement>();
+            Mock.Get(forStatement)
+               .Setup(t => t.Initializer.Expressions)
+               .Returns(new TreeNodeCollection<ICSharpExpression>(new ICSharpExpression[0]));
+            Mock.Get(forStatement)
+                .Setup(t => t.Iterators.Expressions)
+                .Returns(new TreeNodeCollection<ICSharpExpression>(new[] { iterator }));
+            var eater = Mock.Of<IEater>(t => t.Eat(snapshot.Object, iterator) == ExpressionKind.None);
+            var forEater = new ForStatementEater(eater);
+
+            // Act
+            forEater.Eat(snapshot.Object, forStatement);
+
+            // Assert
+            snapshot.Verify(t => t.AddTreeNode(ExpressionKind.None, iterator), Times.Once);
+        }
+    }
+}
---
+++ MockMetrics/MockMetrics.Eating/Statement/ForStatementEater.cs
@@ -0,0 +1,32 @@
+﻿using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace MockMetrics.Eating.Statement
+{
+    public class ForStatementEater : StatementEater<IForStatement>
+    {
+        public ForStatementEater(IEater eater) : base(eater)
+        {
+        }
+
+        public override void Eat(ISnapshot snapshot, IForStatement statement)
+        {
+            Eater.Eat(snapshot, statement.Body);
+
+            var condKind = Eater.Eat(snapshot, statement.Condition);
+            snapshot.AddTreeNode(condKind, statement.Condition);
+
+            foreach (var initializer in statement.Initializer.Expressions)
+            {
+                var kind = Eater.Eat(snapshot, initializer);
+                snapshot.AddTreeNode(kind, initializer);
+            }
+
+            foreach (var iterator in statement.Iterators.Expressions)
+            {
+                var kind = Eater.Eat(snapshot, iterator);
+                snapshot.AddTreeNode(kind, iterator);
+            }
+        }
+    }
+}
