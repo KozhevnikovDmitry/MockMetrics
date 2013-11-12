d021813e24f69feb738beb70184faada4ed92186
KozhevnikovDmitry <d.o.kozhevnikov@gmail.com>
2013-11-12 11:58:08 +0800

    [+] UncheckedStatementEater is implemented

---
+++ MockMetrics/MockMetrics.Eating.Test/Statement/UncheckedStatementEaterTests.cs
@@ -0,0 +1,28 @@
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using MockMetrics.Eating.Statement;
+using Moq;
+using NUnit.Framework;
+
+namespace MockMetrics.Eating.Test.Statement
+{
+    [TestFixture]
+    public class UncheckedStatementEaterTests
+    {
+        [Test]
+        public void EatBodyTest()
+        {
+            // Arrange
+            var snapshot = Mock.Of<ISnapshot>();
+            var body = Mock.Of<IBlock>();
+            var uncheckedStatement = Mock.Of<IUncheckedStatement>(t => t.Body == body);
+            var eater = new Mock<IEater>();
+            var uncheckedStatementEater = new UncheckedStatementEater(eater.Object);
+
+            // Act
+            uncheckedStatementEater.Eat(snapshot, uncheckedStatement);
+
+            // Assert
+            eater.Verify(t => t.Eat(snapshot, body), Times.Once);
+        }
+    }
+}
---
+++ MockMetrics/MockMetrics.Eating/Statement/UncheckedStatementEater.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace MockMetrics.Eating.Statement
+{
+    public class UncheckedStatementEater : StatementEater<IUncheckedStatement>
+    {
+        public UncheckedStatementEater(IEater eater) : base(eater)
+        {
+        }
+
+        public override void Eat(ISnapshot snapshot, IUncheckedStatement statement)
+        {
+            Eater.Eat(snapshot, statement.Body);
+        }
+    }
+}
