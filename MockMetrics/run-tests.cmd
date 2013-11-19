959ede791443768ad09c84c7e9dca91be8bdad4c
KozhevnikovDmitry <d.o.kozhevnikov@gmail.com>
2013-11-19 14:26:41 +0800

    [+] unit-tests launching is added to build script

--- MockMetrics/run-tests.cmd
+++ MockMetrics/run-tests.cmd
@@ -1,2 +1,3 @@
 msbuild MockMetrics\MockMetrics.sln
+nunit-console.exe MockMetrics\MockMetrics.Eating.Tests\bin\Debug\MockMetrics.Eating.Tests.dll
 nunit-console.exe MockMetrics\MockMetrics.Tests\bin\Debug\MockMetrics.Tests.dll
