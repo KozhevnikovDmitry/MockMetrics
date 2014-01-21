93445d90114aaa4b099231bf876a13ce7deb4228
KozhevnikovDmitry <d.o.kozhevnikov@gmail.com>
2014-01-21 14:23:21 +0800

    [+] test for internal variables is added

---
+++ MockMetrics/test/data/daemon/Tested/Tested.Tests/AggregatorTests/InternalVariableTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Tested.Tests.AggregatorTests
+{
+    [TestFixture]
+    public class InternalVariableTests
+    {
+        private Aggregator Aggregator { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Aggregator = new Aggregator(null, null);
+        }
+
+        [Test]
+        public void InternalVariableTestTest()
+        {
+            var result = Aggregator.Aggregate("sdfsdf");
+
+            Assert.AreEqual(result, 1);
+        }
+    }
+}
