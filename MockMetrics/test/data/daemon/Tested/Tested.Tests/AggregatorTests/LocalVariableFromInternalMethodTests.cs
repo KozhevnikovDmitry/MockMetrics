28ad676b619ecc9c89993fc931a379c1e2e70ffa
KozhevnikovDmitry <d.o.kozhevnikov@gmail.com>
2014-01-22 08:46:35 +0800

    [+] Test#12 - target from internal method - is added

---
+++ MockMetrics/test/data/daemon/Tested/Tested.Tests/AggregatorTests/LocalVariableFromInternalMethodTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Tested.Tests.AggregatorTests
+{
+    [TestFixture]
+    public class LocalVariableFromInternalMethodTests
+    {
+        private Aggregator GetAggregator()
+        {
+            return null;
+        }
+
+        [Test]
+        public void LocalVariableFromInternalMethodTest()
+        {
+            var result = GetAggregator();
+
+            Assert.AreEqual(result, 1);
+        }
+    }
+}
