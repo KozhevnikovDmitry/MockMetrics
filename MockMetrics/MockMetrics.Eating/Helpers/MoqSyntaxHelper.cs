cdda41f5a33d6015bf29e95385c84a3ee5252650
KozhevnikovDmitry <d.o.kozhevnikov@gmail.com>
2014-01-22 12:04:08 +0800

    [+] more safe concept for identify Moq method

--- MockMetrics/MockMetrics.Eating/Helpers/MoqSyntaxHelper.cs
+++ MockMetrics/MockMetrics.Eating/Helpers/MoqSyntaxHelper.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Linq;
 using JetBrains.Annotations;
+using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Modules;
 using MockMetrics.Eating.MoqFake;
 
 namespace MockMetrics.Eating.Helpers
@@ -16,61 +19,93 @@ namespace MockMetrics.Eating.Helpers
             _eatExpressionHelper = eatExpressionHelper;
         }
 
-        public virtual MoqInvoke? GetMoqInvokeType(IInvocationExpression expression)
+        private string[] ICallback = { "Callback" };
+        private string[] IThrows = { "Throws" };
+        private string[] MockMock = { "Get" };
+        private string[] MockFakeProperty = { "SetupGet", "SetupSet", "VerifyGet", "VerifySet", "SetupProperty", "SetupAllProperties" };
+        private string[] MockFakeWithOptions = { "Setup", "Verify" };
+        private string[] MockStubWithOptions = { "Of" };
+        private string[] ItFakeWithoutOptions = { "IsAny", "IsIn", "IsInRange", "IsNotIn", "IsRegex", "IsNotNull" };
+        private string[] ItStubWithOptions = { "Is" };
+
+
+        public virtual MoqInvoke? GetMoqInvokeType([NotNull] IInvocationExpression expression)
         {
-            var invokedName = _eatExpressionHelper.GetInvokedElementName(expression);
-
-            if (invokedName.Equals("Method:Moq.Mock.Get(T mocked)"))
+            if (expression == null) throw new ArgumentNullException("expression");
+            
+            var clrElement = _eatExpressionHelper.GetInvokedElement(expression) as IClrDeclaredElement;
+            if (clrElement == null)
             {
-                return MoqInvoke.Mock;
+                return MoqInvoke.None;
+            } 
+            
+            var module = clrElement.Module;
+            if (module.Name != "Moq")
+            {
+                return MoqInvoke.None;
             }
-
-            if (invokedName.Equals("Method:Moq.Language.IReturns`2.Returns(TResult value)"))
+            var type = clrElement.GetContainingType();
+            if (type == null)
             {
                 return MoqInvoke.None;
             }
 
-            if (invokedName.Equals("Method:Moq.Language.ICallback`2.Callback(System.Action action)") ||
-                invokedName.Equals("Method:Moq.Language.ICallback.Callback(System.Action action)"))
-            {
-                return MoqInvoke.FakeCallback;
-            }
+            var elementName = clrElement.ShortName;
+            var typeName = type.GetClrName().FullName;
 
-            if (invokedName.StartsWith("Method:Moq.Language.IThrows.Throws"))
+            if (module.Name == "Moq")
             {
-                return MoqInvoke.FakeException;
-            }
+                if (typeName == "Moq.Mock" || typeName == "Moq.Mock`1")
+                {
+                    if (MockMock.Contains(elementName))
+                    {
+                        return MoqInvoke.Mock;
+                    }
 
-            if (invokedName.StartsWith("Method:Moq.Mock`1.SetupGet") ||
-                invokedName.StartsWith("Method:Moq.Mock`1.SetupSet") ||
-                invokedName.StartsWith("Method:Moq.Mock`1.VerifyGet") ||
-                invokedName.StartsWith("Method:Moq.Mock`1.VerifySet") ||
-                invokedName.StartsWith("Method:Moq.Mock`1.SetupProperty") ||
-                invokedName.StartsWith("Method:Moq.Mock`1.SetupAllProperties"))
-            {
-                return MoqInvoke.FakeProperty;
-            }
+                    if (MockFakeProperty.Contains(elementName))
+                    {
+                        return MoqInvoke.FakeProperty;
+                    }
 
-            if (invokedName.StartsWith("Method:Moq.It.IsAny") ||
-                invokedName.StartsWith("Method:Moq.It.IsIn") ||
-                invokedName.StartsWith("Method:Moq.It.IsInRange") ||
-                invokedName.StartsWith("Method:Moq.It.IsNotIn") ||
-                invokedName.StartsWith("Method:Moq.It.IsRegex") ||
-                invokedName.StartsWith("Method:Moq.It.IsNotNull"))
-            {
-                return MoqInvoke.FakeWithoutOptions;
-            }
+                    if (MockFakeWithOptions.Contains(elementName))
+                    {
+                        return MoqInvoke.FakeWithOptions;
+                    }
 
-            if (invokedName.StartsWith("Method:Moq.Mock`1.Setup") ||
-                invokedName.StartsWith("Method:Moq.Mock`1.Verify"))
-            {
-                return MoqInvoke.FakeWithOptions;
-            }
+                    if (MockStubWithOptions.Contains(elementName))
+                    {
+                        return MoqInvoke.StubWithOptions;
+                    }
+                }
 
-            if (invokedName.StartsWith("Method:Moq.Mock.Of") ||
-                invokedName.StartsWith("Method:Moq.It.Is"))
-            {
-                return MoqInvoke.StubWithOptions;
+                if (typeName == "Moq.Language.IThrows")
+                {
+                    if (IThrows.Contains(elementName))
+                    {
+                        return MoqInvoke.FakeException;
+                    }
+                }
+
+                if (typeName == "Moq.Language.ICallback`2" || typeName == "Moq.Language.ICallback")
+                {
+                    if (ICallback.Contains(elementName))
+                    {
+                        return MoqInvoke.FakeCallback;
+                    }
+                }
+
+                if (typeName == "Moq.It")
+                {
+                    if (ItFakeWithoutOptions.Contains(elementName))
+                    {
+                        return MoqInvoke.FakeWithoutOptions;
+                    }
+
+                    if (ItStubWithOptions.Contains(elementName))
+                    {
+                        return MoqInvoke.StubWithOptions;
+                    }
+                }
             }
 
             return MoqInvoke.None;
