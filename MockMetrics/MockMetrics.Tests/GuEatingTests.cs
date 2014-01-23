ff9a450f97d7e217dc9a099db8683e1661044e16
KozhevnikovDmitry <d.o.kozhevnikov@gmail.com>
2014-01-23 14:46:07 +0800

    [+] gu eating tests reorganized by namespaces

--- MockMetrics/MockMetrics.Tests/GuEatingTests.cs
+++ MockMetrics/MockMetrics.Tests/GuEatingTests.cs
@@ -38,13 +38,49 @@ namespace MockMetrics.Tests
             get { return @"GU.MZ.sln"; }
         }
 
-        [TestCase(@"<GU.MZ.BL.Tests>\DataMapping\LicenseHolderDataMapperTests")]
-        public void GU_MZ_BL_Tests(string testName)
+        [TestCase(@"<GU.MZ.BL.Tests>\DataMapping")]
+        public void GU_MZ_BL_DataMapping_Tests(string testName)
         {
             DoMultipleTestFiles(testName);
             var snapshots = FakesElementProcessor.Results.Values;
             Console.WriteLine(snapshots.Count());
-            new SnapshotDump().Dump(snapshots, "gu_mz_bl");
+            new SnapshotDump().Dump(snapshots, "gu_mz_bl_data_mapping");
+        }
+
+        [TestCase(@"<GU.MZ.BL.Tests>\DomainLogic")]
+        public void GU_MZ_BL_DomainLogic_Tests(string testName)
+        {
+            DoMultipleTestFiles(testName);
+            var snapshots = FakesElementProcessor.Results.Values;
+            Console.WriteLine(snapshots.Count());
+            new SnapshotDump().Dump(snapshots, "gu_mz_bl_domain_logic");
+        }
+
+        [TestCase(@"<GU.MZ.BL.Tests>\Validation")]
+        public void GU_MZ_BL_Validation_Tests(string testName)
+        {
+            DoMultipleTestFiles(testName);
+            var snapshots = FakesElementProcessor.Results.Values;
+            Console.WriteLine(snapshots.Count());
+            new SnapshotDump().Dump(snapshots, "gu_mz_bl_validation");
+        }
+
+        [TestCase(@"<GU.MZ.BL.Tests>\Reporting")]
+        public void GU_MZ_BL_Reporting_Tests(string testName)
+        {
+            DoMultipleTestFiles(testName);
+            var snapshots = FakesElementProcessor.Results.Values;
+            Console.WriteLine(snapshots.Count());
+            new SnapshotDump().Dump(snapshots, "gu_mz_bl_reporting");
+        }
+
+        [TestCase(@"<GU.MZ.BL.Tests>\Common")]
+        public void GU_MZ_BL_Common_Tests(string testName)
+        {
+            DoMultipleTestFiles(testName);
+            var snapshots = FakesElementProcessor.Results.Values;
+            Console.WriteLine(snapshots.Count());
+            new SnapshotDump().Dump(snapshots, "gu_mz_bl_common");
         }
     }
 }
