36758be0db35ee307cd5b90ec466990e836891fa
KozhevnikovDmitry <d.o.kozhevnikov@gmail.com>
2014-01-24 11:03:33 +0800

    [-] exclude launching of acceptance from build script

--- MockMetrics/run-tests.cmd
+++ MockMetrics/run-tests.cmd
@@ -1,3 +1,2 @@
 msbuild MockMetrics\MockMetrics.sln
 nunit-console.exe MockMetrics\MockMetrics.Eating.Tests\bin\Debug\MockMetrics.Eating.Tests.dll
-nunit-console.exe MockMetrics\MockMetrics.Tests\bin\Debug\MockMetrics.Tests.dll
