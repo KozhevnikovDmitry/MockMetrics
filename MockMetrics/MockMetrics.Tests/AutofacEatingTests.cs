fd6967827bc893eed40a98fd8affd0f64f1bbde7
KozhevnikovDmitry <d.o.kozhevnikov@gmail.com>
2014-01-28 10:41:23 +0800

    [+] eating autofac tests assembly

---
+++ MockMetrics/MockMetrics.Tests/AutofacEatingTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using JetBrains.Application.Settings;
+using JetBrains.ReSharper.Daemon;
+using MockMetrics.Fake;
+using NUnit.Framework;
+
+namespace MockMetrics.Tests
+{
+    public class AutofacEatingTests : BaseFixture
+    {
+        protected override bool HighlightingPredicate(IHighlighting highlighting,
+            IContextBoundSettingsStore settingsstore)
+        {
+            return highlighting is FakeHighlighting;
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            FakesElementProcessor.Results.Clear();
+        }
+
+        [TearDown]
+        public override void TearDown()
+        {
+            base.TearDown();
+            FakesElementProcessor.Results.Clear();
+        }
+
+        protected override string RelativeTestDataPath
+        {
+            get { return @"D:\GitHub\Autofac"; }
+        }
+
+        protected override string SolutionName
+        {
+            get { return @"Autofac.sln"; }
+        }
+
+        [TestCase(@"<Autofac.Tests>")]
+        [TestCase(@"<Core.Tests>")]
+        public void Autofac_Tests_Tests(string testName)
+        {
+            DoMultipleTestFiles(testName);
+            var snapshots = FakesElementProcessor.Results.Values;
+            Console.WriteLine(snapshots.Count());
+            new SnapshotDump().Dump(snapshots, "autofac_tests");
+        }
+    }
+}
