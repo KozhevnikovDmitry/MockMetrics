ace7469e2cfb3e7cd5d45f56a4e9dafc6d663972
KozhevnikovDmitry <d.o.kozhevnikov@gmail.com>
2014-02-06 12:32:10 +0800

    [+] postbuild generate gold files for integration tests

--- MockMetrics/post-build.cmd
+++ MockMetrics/post-build.cmd
@@ -8,3 +8,5 @@ copy C:\projects\mockmetrics\MockMetrics\packages\JetBrains.ReSharper.SDK.8.1.55
 
 copy C:\projects\mockmetrics\MockMetrics\packages\JetBrains.ReSharper.SDK.8.1.555\bin\x86 C:\projects\mockmetrics\MockMetrics\MockMetrics.Tests\bin\Release\x86
 copy C:\projects\mockmetrics\MockMetrics\packages\JetBrains.ReSharper.SDK.8.1.555\bin\x64 C:\projects\mockmetrics\MockMetrics\MockMetrics.Tests\bin\Release\x64
+
+C:\projects\mockmetrics\MockMetrics\test\data\daemon\goldgen.exe
